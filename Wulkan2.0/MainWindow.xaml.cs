7aa0c6d9734f114f61a44526f00b4837999c4035
adi927 <jaskolskiadam41@gmail.com>
2023-11-04 23:37:20 +0100

    Init

---
+++ Wulkan2.0/MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlTypes;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+using Newtonsoft.Json;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace uczenSredniaWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int aktualnyUczenIndex = 0;
+        public int iloscUczniow;
+        List<Uczen> listaUczniow = new List<Uczen>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            string sciezka = @"daneUczniowPrzykladowe.json";
+            string json = File.ReadAllText(sciezka);
+
+
+            List<double> srednieUczniow = new List<double>();
+            listaUczniow = JsonConvert.DeserializeObject<List<Uczen>>(json);
+
+            iloscUczniow = (listaUczniow.Count - 1);
+            int liczbaUczniow = listaUczniow.Count;
+
+            iloscUczniowLabel.Content = "Ilosc uczniow: " + liczbaUczniow.ToString();
+
+            int uczenZnajniszaSrednia = 0, uczenZnajwyszaSrednia = 0;
+            double najwyzszaSrednia = int.MinValue;
+            double najnizszaSrednia = int.MaxValue;
+
+            for (int i = 0; i < listaUczniow.Count; i++)
+            {
+                srednieUczniow.Add(listaUczniow[i].SredniaOcenUcznia());
+            }
+
+            for (int i = 0; i < srednieUczniow.Count; i++)
+            {
+                if (srednieUczniow[i] > najwyzszaSrednia)
+                {
+                    najwyzszaSrednia = srednieUczniow[i];
+                    uczenZnajwyszaSrednia = i;
+                }
+            }
+
+            for (int i = 0; i < srednieUczniow.Count; i++)
+            {
+                if (srednieUczniow[i] < najnizszaSrednia)
+                {
+                    najnizszaSrednia = srednieUczniow[i];
+                    uczenZnajniszaSrednia = i;
+                }
+            }
+            WyswietlDaneUcznia(aktualnyUczenIndex);
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (aktualnyUczenIndex > 0)
+            {
+                aktualnyUczenIndex--;
+            }
+            else
+            {
+                aktualnyUczenIndex = listaUczniow.Count - 1;
+            }
+
+            WyswietlDaneUcznia(aktualnyUczenIndex);
+        }
+
+        private void btnLeft_Click(object sender, RoutedEventArgs e)
+        {
+            if (aktualnyUczenIndex < listaUczniow.Count - 1)
+            {
+                aktualnyUczenIndex++;
+            }
+            else
+            {
+                aktualnyUczenIndex = 0;
+            }
+
+            WyswietlDaneUcznia(aktualnyUczenIndex);
+        }
+
+        private void WyswietlDaneUcznia(int index)
+        {
+            imieLabel.Content = "IMIE: " + listaUczniow[index].Imie();
+            nazwiskoLabel.Content = "NAZWISKO: " + listaUczniow[index].Nazwisko();
+            dataLabel.Content = "DATA URODZENIA: " + listaUczniow[index].Data();
+            klasaLabel.Content = "KLASA: " + listaUczniow[index].Klasa();
+            semestrLabel.Content = "SEMESTR: " + listaUczniow[index].Semestr();
+            peselBlock.Text = listaUczniow[index].Pesel();
+            ocenyBlock.Text = listaUczniow[index].Oceny();
+            sredniaBlock.Text = listaUczniow[index].Srednia;
+
+            try
+            {
+                string imagePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ico", listaUczniow[index].Zdjecie);
+
+                if (!File.Exists(imagePath))
+                {
+                    MessageBox.Show("Brak zdjęcia dla ucznia: " + listaUczniow[index].Imie() + " " + listaUczniow[index].Nazwisko());
+                    MessageBox.Show(imagePath);
+                }
+                else
+                {
+                    BitmapImage bitmap = new BitmapImage(new Uri(imagePath));
+                    studentImage.Source = bitmap;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Wystąpił błąd: {ex.Message}");
+            }
+
+
+        }
+    }
+
+}
---
+++ Wulkan2.0/uczenSredniaWPF.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uczenSredniaWPF
+{
+    internal class Uczen
+    {
+        private string imie;
+        private string nazwisko;
+        private string klasa;
+        private int semestr;
+        private double srednia;
+        private string pesel;
+        private DateTime data_ur;
+        private List<int> oceny;
+
+        public Uczen(string pesel, string imie, string zdjecie, string nazwisko, string klasa, int semestr, List<int> oceny)
+        {
+            this.pesel = pesel;
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.klasa = klasa;
+            this.Zdjecie = zdjecie;
+            this.semestr = semestr;
+            this.oceny = oceny;
+            this.data_ur = Data_ur();
+            this.srednia = SredniaOcenUcznia();
+        }
+
+        public string Zdjecie { get; set; }
+
+        public string Srednia
+        {
+            get { return Math.Round(srednia, 2).ToString("F2"); }
+            private set { srednia = double.Parse(value); }
+        }
+
+        public int IloscOcen()
+        {
+            return oceny.Count;
+        }
+
+        public double SredniaOcenUcznia()
+        {
+            double srednia = 0;
+            int sumaOcen = 0;
+
+            foreach (int i in oceny)
+            {
+                sumaOcen += i;
+            }
+            srednia = (double)sumaOcen / IloscOcen();
+
+            Srednia = srednia.ToString();
+
+            return Math.Round(srednia, 2);
+        }
+
+
+        public DateTime Data_ur()
+        {
+            int rok = int.Parse(pesel.Substring(0, 2));
+            int miesiac = int.Parse(pesel.Substring(2, 2));
+            int dzien = int.Parse(pesel.Substring(4, 2));
+
+            if (miesiac >= 81 && miesiac <= 92)
+            {
+                rok += 1800;
+                miesiac -= 80;
+            }
+            else if (miesiac >= 1 && miesiac <= 12)
+            {
+                rok += 1900;
+            }
+            else if (miesiac >= 21 && miesiac <= 32)
+            {
+                rok += 2000;
+                miesiac -= 20;
+            }
+            else if (miesiac >= 41 && miesiac <= 52)
+            {
+                rok += 2100;
+                miesiac -= 40;
+            }
+            else if (miesiac >= 61 && miesiac <= 72)
+            {
+                rok += 2200;
+                miesiac -= 60;
+            }
+
+            return new DateTime(rok, miesiac, dzien);
+        }
+
+        public string Data()
+        {
+            return data_ur.ToShortDateString();
+        }
+
+        public string Imie()
+        {
+            return imie;
+        }
+
+        public string Nazwisko()
+        {
+            return nazwisko;
+        }
+
+        public string Klasa()
+        {
+            return klasa;
+        }
+
+        public string Pesel()
+        {
+            return pesel;
+        }
+
+        public int Semestr()
+        {
+            return semestr;
+        }
+
+        public string Oceny()
+        {
+            string oceny = string.Join(" ", this.oceny);
+            return oceny;
+        }
+
+        
+    }
+}
